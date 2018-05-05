154d8eca5db5b882d7c037e3b5753c1255b4c30a
stephane de la torre <s.dlt@hotmail.fr>
2018-05-05 19:30:14 +0200

    session 5/66 done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require("electron");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow, tray;
 
@@ -11,5 +11,9 @@ app.on("ready", () => {
     webPreferences: { backgroundThrottling: false }
   });
 
-  mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on("videos:added", (event, videos) => {
+  console.log(videos);
 });
